c8e5a8caf4c48c8935872c56f00061dc7377abc2
Tymofii Kononenko <timajw95@gmail.com>
2024-11-02 01:23:08 +0100

    tasks done

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,15 @@
+const body = document.body;
+const colorSpan = document.querySelector('.color');
+const changeColorButton = document.querySelector('.change-color');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+changeColorButton.addEventListener('click', () => {
+  const newRandomColor = getRandomHexColor();
+  body.style.backgroundColor = newRandomColor;
+  colorSpan.textContent = newRandomColor;
+});
